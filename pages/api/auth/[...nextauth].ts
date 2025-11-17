88dc194c1955eb2788bce05a3be6efea9b6f2125
ironacregym-bit <ironacregym@gmail.com>
2025-11-17 11:02:33 +0000

    Create [...nextauth].ts

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  })],
+  callbacks: {
+    async session({ session }: any) { return session; }
+  }
+};
+export default NextAuth(authOptions as any);
