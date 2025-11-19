51132be465183cbd3e3f9bbba948ccd5cbcbae69
ironacregym-bit <ironacregym@gmail.com>
2025-11-19 11:56:45 +0000

    Update sheets.ts

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -22,11 +22,12 @@ if (!PRIVATE_KEY.includes("BEGIN PRIVATE KEY") || !PRIVATE_KEY.endsWith("-----EN
   console.error("GOOGLE_PRIVATE_KEY invalid format (missing BEGIN/END or newlines).");
 }
 
+
 const auth = new google.auth.JWT(
-  CLIENT_EMAIL,
+  process.env.GOOGLE_CLIENT_EMAIL,
   undefined,
-  PRIVATE_KEY,
-  ["https://www.googleapis.com/auth/spreadsheets"]
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/spreadsheets']
 );
 
 const sheets = google.sheets({ version: "v4", auth });
