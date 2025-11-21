8d5c25114f32c442f55a12a077687c48a6363aa4
ironacregym-bit <ironacregym@gmail.com>
2025-11-21 16:54:11 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import Link from "next/link";
 import { useEffect } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import BottomNav from "../components/BottomNav";
 
 const fetcher = (u: string) => fetch(u).then((r) => r.json());
 
@@ -120,31 +121,7 @@ export default function Home() {
       </main>
 
       {/* Bottom Navigation */}
-      <nav className="navbar fixed-bottom bg-light border-top">
-        <div className="container d-flex justify-content-around">
-          <Link href="/">
-            <i className="fas fa-home fa-lg"></i>
-            <div style={{ fontSize: "12px" }}>Home</div>
-          </Link>
-          <Link href="/workout/today">
-            <i className="fas fa-dumbbell fa-lg"></i>
-            <div style={{ fontSize: "12px" }}>WoD</div>
-          </Link>
-          <Link href="/profile">
-            <i className="fas fa-user fa-lg"></i>
-            <div style={{ fontSize: "12px" }}>Profile</div>
-          </Link>
-          <a
-            href={`https://wa.me/${process.env.NEXT_PUBLIC_TRAINER_PHONE || process.env.TRAINER_PHONE}?text=Hi%20Coach%20I%27m%20doing%20BXKR`}
-            target="_blank"
-            rel="noreferrer"
-            className="text-center text-dark"
-          >
-            <i className="fas fa-comments fa-lg"></i>
-            <div style={{ fontSize: "12px" }}>Chat</div>
-          </a>
-        </div>
-      </nav>
+      <BottomNav />
     </>
   );
 }
