f88ce86e6bc54a113f4a8a7b42c093be77d5ac55
ironacregym-bit <ironacregym@gmail.com>
2025-11-21 15:50:43 +0000

    Update [id].tsx

--- pages/workout/[id].tsx
+++ pages/workout/[id].tsx
@@ -118,8 +118,9 @@ export default function WorkoutPage() {
     <>
       <Head>
         <title>{workout.title} - BXKR</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/css/all.min.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
       </Head>
 
 
