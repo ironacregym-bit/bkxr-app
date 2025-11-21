2a074966fa805e30b27ea459a3a12b8b0297a860
ironacregym-bit <ironacregym@gmail.com>
2025-11-21 11:02:13 +0000

    Update profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -41,7 +41,7 @@ export default function Profile() {
         <title>Profile | BXKR</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
       </Head>
 
       <main className="container d-flex justify-content-center align-items-center" style={{ minHeight: "80vh", paddingBottom: "70px" }}>
