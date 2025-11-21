fd56bf07ceac00c8b038726787b361b46079f389
ironacregym-bit <ironacregym@gmail.com>
2025-11-21 08:49:54 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,8 +46,8 @@ export default function Home() {
     <>
       <Head>
         <title>BXKR</title>
-        <link rel="stylesheet" href="https://cdn.3.2/dist/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="https://cloudfare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
       </Head>
 
       <main className="container py-3">
