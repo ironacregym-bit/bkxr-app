994b4feb62b6e7df21d2e93039a7411ab1c03b40
ironacregym-bit <ironacregym@gmail.com>
2025-11-24 11:41:06 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "../styles/bootstrap.css";
 export default function App({ Component, pageProps }: AppProps) {
   return <SessionProvider><Component {...pageProps} /></SessionProvider>;
 }
