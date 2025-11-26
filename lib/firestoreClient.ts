24fb9bac90ab9b2375b3473558e9705b4ba4f28b
ironacregym-bit <ironacregym@gmail.com>
2025-11-26 16:28:36 +0000

    Update firestoreClient.ts

--- lib/firestoreClient.ts
+++ lib/firestoreClient.ts
@@ -3,7 +3,7 @@ import { Firestore } from "@google-cloud/firestore";
 const firestore = new Firestore({
   projectId: process.env.GOOGLE_PROJECT_ID,
   credentials: {
-    client_email: process.env.GOOGLE_CLIENT_IDL,
+    client_email: process.env.GOOGLE_CLIENT_ID,
     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   },
 });
