b8b0485f40285099aa1126a4c8ace15b1ead8f6e
ironacregym-bit <ironacregym@gmail.com>
2025-11-26 16:28:23 +0000

    Update firestoreClient.ts

--- lib/firestoreClient.ts
+++ lib/firestoreClient.ts
@@ -1,7 +1,7 @@
 import { Firestore } from "@google-cloud/firestore";
 
 const firestore = new Firestore({
-  projectId: process.env.GCP_PROJECT_ID,
+  projectId: process.env.GOOGLE_PROJECT_ID,
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_IDL,
     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
