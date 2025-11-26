d020cdd63a5f84f4c65ba68c10763696d23e74d2
ironacregym-bit <ironacregym@gmail.com>
2025-11-26 11:50:34 +0000

    Update and rename firebaseConfig.ts to firebaseClient.ts

---
+++ lib/firebaseClient.ts
@@ -0,0 +1,11 @@
+import { Firestore } from "@google-cloud/firestore";
+
+const firestore = new Firestore({
+  projectId: process.env.GCP_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_IDL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  },
+});
+
+export default firestore;
