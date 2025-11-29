4d1f0b162d46709b96781dcf5d9bbea2891ca6f5
ironacregym-bit <ironacregym@gmail.com>
2025-11-29 11:06:06 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <addtohomescreen />
     </SessionProvider>
   );
 }
