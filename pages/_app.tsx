7734dbe3e58b00bd1d0c2154b5453506156dec1f
ironacregym-bit <ironacregym@gmail.com>
2025-11-29 07:50:24 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/bootstrap.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <SessionProvider><Component {...pageProps} /></SessionProvider>;
+  useEffect(() => {
+    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .catch((err) =>
+          console.error("Service worker registration failed:", err)
+        );
+    }
+  }, []);
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
