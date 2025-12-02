0865f8d0b500af87e5cb6e9d55a490fccf36d2a1
ironacregym-bit <ironacregym@gmail.com>
2025-12-02 19:11:14 +0000

    Update logs.ts

--- pages/api/nutrition/logs.ts
+++ pages/api/nutrition/logs.ts
@@ -2,7 +2,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]"; // adjust path if needed
-import firestore from "@/lib/firestoreClient";
+import firestore from "../../lib/firestoreClient";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions);
