5cd22e1b8814261f31c42110200646d3e5c7e32a
ironacregym-bit <ironacregym@gmail.com>
2025-12-02 18:34:19 +0000

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,7 +1,24 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
-  interface Session {
+  interface User extends DefaultUser {
+    role?: string;
+    gym_id?: string | null;
+  }
+
+  interface Session extends DefaultSession {
     accessToken?: string;
+    user?: {
+      role?: string;
+      gym_id?: string | null;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+    role?: string;
+    gym_id?: string | null;
   }
 }
