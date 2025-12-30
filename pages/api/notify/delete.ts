b5bd7e94a92069f6d79a80578d011cdbad973403
ironacregym-bit <ironacregym@gmail.com>
2025-12-30 08:20:49 +0000

    Update delete.ts

--- pages/api/notify/delete.ts
+++ pages/api/notify/delete.ts
@@ -2,8 +2,8 @@
 // pages/api/notify/templates/delete.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]";
-import firestore from "../../../../lib/firestoreClient";
+import { authOptions } from "../auth/[...nextauth]";
+import firestore from "../../../lib/firestoreClient";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") { res.setHeader("Allow", "POST"); return res.status(405).json({ error: "Method not allowed" }); }
