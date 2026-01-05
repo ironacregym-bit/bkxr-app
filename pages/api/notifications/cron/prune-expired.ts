2aab8e26ee2d66a27e5a7a8ec052ffc641133060
ironacregym-bit <ironacregym@gmail.com>
2026-01-05 15:59:10 +0000

    Update prune-expired.ts

--- pages/api/notifications/cron/prune-expired.ts
+++ pages/api/notifications/cron/prune-expired.ts
@@ -1,6 +1,6 @@
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import firestore from "../../../lib/firestoreClient";
+import firestore from "../../../../lib/firestoreClient";
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
   try {
