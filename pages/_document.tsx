979decbf9f8d46132523fe6530e9ffe701bd0ee5
ironacregym-bit <ironacregym@gmail.com>
2026-01-14 07:47:37 +0000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="mobile-web-app-capable" content="yes" />
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
 
