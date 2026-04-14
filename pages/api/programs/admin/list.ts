d39eaa73e9b0090fc9f80153f3f40832ac8a2457
ironacregym-bit <ironacregym@gmail.com>
2026-04-14 08:00:04 +0100

    Update list.ts

--- pages/api/programs/admin/list.ts
+++ pages/api/programs/admin/list.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import firestore from "../../../lib/firestoreClient";
+import firestore from "../../../../lib/firestoreClient";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
+import { authOptions } from "../../auth/[...nextauth]";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions).catch(() => null);
