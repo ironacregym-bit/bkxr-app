5be37ae8861a8b9ad619063f6ea5edea0f8af22e
ironacregym-bit <ironacregym@gmail.com>
2026-05-18 17:16:06 +0100

    Update gym-create.tsx

--- pages/admin/workouts/gym-create.tsx
+++ pages/admin/workouts/gym-create.tsx
@@ -1,7 +1,7 @@
 // File: pages/admin/workouts/gym-create.tsx
 
 import Head from "next/head";
-import Link from "next };import Link from "next/link";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import useSWR from "swr";
