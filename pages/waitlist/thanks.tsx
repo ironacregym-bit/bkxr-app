8c741bfa8f469e58a28f491f39efc9b92f14355a
ironacregym-bit <ironacregym@gmail.com>
2026-05-20 11:00:21 +0100

    Update thanks.tsx

--- pages/waitlist/thanks.tsx
+++ pages/waitlist/thanks.tsx
@@ -1,5 +1,5 @@
 // File: pages/waitlist/thanks.tsx
-import Head from from "next/head";
+import Head from "next/head";
 import Link from "next/link";
 import { useMemo } from "react";
 import { useRouter } from "next/router";
