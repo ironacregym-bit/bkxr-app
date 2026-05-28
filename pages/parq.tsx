1638ff59a30a7282f617dc9c5098c1ee88b81c88
ironacregym-bit <ironacregym@gmail.com>
2026-05-28 10:14:45 +0100

    Update parq.tsx

--- pages/parq.tsx
+++ pages/parq.tsx
@@ -1,9 +1,9 @@
 // File: pages/parq.tsx
+import Link from "next/link"
 import Head from "next/head";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
 import { useEffect, useId, useMemo, useRef, useState, type FormEvent } from "react";
 import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 type Answer = "yes" | "no" | "";
 
