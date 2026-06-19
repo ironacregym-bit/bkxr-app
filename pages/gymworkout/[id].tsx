11c735d9b784c02cc48b0a5bea8779533efc88e9
ironacregym-bit <ironacregym@gmail.com>
2026-06-19 08:09:22 +0100

    Update [id].tsx

--- pages/gymworkout/[id].tsx
+++ pages/gymworkout/[id].tsx
@@ -265,7 +265,7 @@ export default function GymWorkoutViewerPage() {
 
   function toggleTick(exercise_id: string, setNum: number) {
     const k = `${exercise_id}|${setNum}`;
-    setTickKeys((m) => ({ ...m, !m[k] }));
+    setTickKeys((m) => ({ ...m, [k]: !m[k] }));
   }
 
   const mediaRounds = useMemo(() => toMediaRounds(data), [data]);
