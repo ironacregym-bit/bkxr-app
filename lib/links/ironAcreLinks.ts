101d7e00584bb0b5d7f41c8bc687b399cefa67f6
ironacregym-bit <ironacregym@gmail.com>
2026-06-25 08:31:13 +0100

    Create ironAcreLinks.ts

---
+++ lib/links/ironAcreLinks.ts
@@ -0,0 +1,13 @@
+// lib/ironAcreLinks.ts
+export const IRON_ACRE_LINKS = {
+  appSignup: "/app-signup",
+  founders: "/founders",
+  gymInterest: "/founders",
+  podcast: "https://www.youtube.com/",
+  socials: {
+    instagram: "#",
+    youtube: "https://www.youtube.com/",
+    facebook: "#",
+    tiktok: "#",
+  },
+} as const;
